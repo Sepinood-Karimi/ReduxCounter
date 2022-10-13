dee4399c4aca2e15941b4fefd82b75a6a5d8cd6d
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-10-13 13:42:42 +0330

    add signup and login logic

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,23 +1,75 @@
 import classes from './Auth.module.css';
+import {useRef, useState} from "react";
 
 
 const Auth = () => {
-    const loginHandler = (event) => {
+
+    const emailInputRef = useRef();
+    const passwordInputRef = useRef();
+
+    const [isLogin, setIsLogin] = useState(true);
+
+    const loginHandler = async (event) => {
         event.preventDefault();
-    }
+
+        const enteredEmail = emailInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+
+        let url;
+        if (isLogin) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCCv9p4SEWJKbVkOPM5e8W9n-RzFaofv84';
+        } else {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCCv9p4SEWJKbVkOPM5e8W9n-RzFaofv84';
+        }
+
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: enteredEmail,
+                    password: enteredPassword,
+                    returnSecureToken: true
+                })
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                let error = data.error.message;
+                throw new Error(error);
+            }
+            if (response.ok) {
+                if (isLogin){
+                    alert('You Successfully Logged In !');
+                }else{
+                    alert('You Successfully Signup !');
+                }
+            }
+        } catch (e) {
+            alert(e.message);
+        }
+    };
+
+    const signupToggle = () => {
+        setIsLogin(false);
+    };
+
     return (
         <main className={classes.auth}>
             <section>
                 <form onSubmit={loginHandler}>
                     <div className={classes.control}>
                         <label htmlFor='email'>Email</label>
-                        <input type='email' id='email'/>
+                        <input type='email' id='email' ref={emailInputRef}/>
                     </div>
                     <div className={classes.control}>
                         <label htmlFor='password'>Password</label>
-                        <input type='password' id='password'/>
+                        <input type='password' id='password' ref={passwordInputRef}/>
                     </div>
-                    <button>Login</button>
+                    <button>{isLogin ? 'Login' : 'Signup'}</button>
+                    <p onClick={signupToggle}> Dont have an account ? create one. </p>
                 </form>
             </section>
         </main>
