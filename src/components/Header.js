b6ea8606da564b07f6cbe235c4fa99576e6c1d13
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-10-13 12:17:13 +0330

    add initial files for authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,25 @@ import classes from './Header.module.css';
 
 const Header = () => {
 
+    const logoutHandler = () => {
+
+    }
     return (
         <header className={classes.header}>
-            <h1>Redux Counter</h1>
+            <h1> Redux Auth & Counter </h1>
+            <nav>
+                    <ul>
+                        <li>
+                            <a href='/'>My Products</a>
+                        </li>
+                        <li>
+                            <a href='/'>My Sales</a>
+                        </li>
+                        <li>
+                            <button onClick={logoutHandler}>Logout</button>
+                        </li>
+                    </ul>
+            </nav>
         </header>
     );
 };
