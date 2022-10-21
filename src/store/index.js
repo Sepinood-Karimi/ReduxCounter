75a232d9bcc207ed5c95fe3bdc8d50b3e4ef9569
Sepinoud Karimi <sepinoodkarimi@gmail.com>
2022-10-21 16:12:23 +0330

    add auth to local Storage with redux persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {counterSlice} from "./counter-slice";
 import uiSlice from "./ui-slice";
 import authSlice from "./auth-slice";
+import storage from 'redux-persist/lib/storage';
+import {persistReducer, persistStore} from "redux-persist";
 
-const store = configureStore({
-    reducer: {
-        counter: counterSlice.reducer,
-        ui: uiSlice.reducer,
-        auth: authSlice.reducer
-    }
+const persistConfig = {
+    key : 'root',
+    storage,
+    whitelist : ['auth']
+};
+const cmReducer = combineReducers({
+    counter: counterSlice.reducer,
+    ui: uiSlice.reducer,
+    auth: authSlice.reducer
 });
 
+const persistedReducer = persistReducer(persistConfig,cmReducer);
+
+
+const store = configureStore({
+    reducer : persistedReducer
+});
+
+
 export default store;
+
+export const persist = persistStore(store);
